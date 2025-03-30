fda1c0eb593bb0a50592646c30af5cc4caf3ee90
Precious Olusola <preciousolusola16@gmail.com>
2025-03-30 09:16:16 +0100

    Ready to show

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main>
       <Homepage/>
-      <Companies/>
+      {/* <Companies/> */}
     </main>
   )
 }
--- src/components/Home/activities.tsx
+++ src/components/Home/activities.tsx
@@ -11,31 +11,34 @@ interface workdata {
 
 const workdata: workdata[] = [
     {
-        imgSrc: '/images/Work/icon-one.svg',
-        heading: 'Create Account',
-        subheading: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry and this',
-        hiddenpara: 'standard dummy text ever since the 1500s, when an unknownprinter took a galley of type and scrambled it to make a type specimen book. It has survived...',
+        imgSrc: "/images/Work/icon-one.svg",
+        heading: "Oracle",
+        subheading: "You're signed in as for the Games session",
+        hiddenpara: "",
     },
     {
-        imgSrc: '/images/Work/icon-two.svg',
-        heading: 'Find your Credit',
-        subheading: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry and this',
-        hiddenpara: 'standard dummy text ever since the 1500s, when an unknownprinter took a galley of type and scrambled it to make a type specimen book. It has survived...',
+        imgSrc: "/images/Work/icon-two.svg",
+        heading: "Accort",
+        subheading:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry and this",
+        hiddenpara:
+            "standard dummy text ever since the 1500s, when an unknownprinter took a galley of type and scrambled it to make a type specimen book. It has survived...",
     },
     {
-        imgSrc: '/images/Work/icon-three.svg',
-        heading: 'Exchange Currency',
-        subheading: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry and this',
-        hiddenpara: 'standard dummy text ever since the 1500s, when an unknownprinter took a galley of type and scrambled it to make a type specimen book. It has survived...',
+        imgSrc: "/images/Work/icon-three.svg",
+        heading: "🍽️ Inter-Level Cooking Competition Overview",
+        subheading:
+            "The Inter-Level Cooking Competition is a fun, engaging, and competitive session between 100, 200, 300, 400, and 500 level.",
+        hiddenpara:
+            "",
     },
     {
-        imgSrc: '/images/Work/icon-three.svg',
-        heading: 'Exchange Currency',
-        subheading: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry and this',
-        hiddenpara: 'standard dummy text ever since the 1500s, when an unknownprinter took a galley of type and scrambled it to make a type specimen book. It has survived...',
+        imgSrc: "/images/Work/icon-three.svg",
+        heading: "Variety Session",
+        subheading: "",
+        hiddenpara: "",
     },
-
-]
+];
 
 export default function Activities() {
     const { user } = useAuth();
@@ -48,17 +51,28 @@ export default function Activities() {
                     const isOdd = workdata.length % 2 !== 0;
                     const isLast = workdata.length - 1 === i;
 
+
                     return (
-                        <div
-                            className={`card-bb p-8 mx-auto ${
-                                isOdd && isLast
-                                    ? "last:col-span-2 last:justify-self-center"
-                                    : ""
-                            }`.trim()}
-                            key={i}
-                            data-last={i === workdata.length - 1}
-                        >
-                            {/* <div className="work-img-bg rounded-full flex justify-center absolute p-6">
+                        <ActivityItem isLast={isOdd && isLast} subtitle={items.subheading} title={items.heading} key={i}/>
+                    )
+                })}
+            </div>
+        </div>
+    );
+};
+
+
+function ActivityItem(props:{isLast?:boolean; title:string; subtitle:string}) {
+    return (
+        <div
+            className={`card-bb p-8 mx-auto ${
+                props.isLast
+                    ? "last:col-span-2 last:justify-self-center"
+                    : ""
+            }`.trim()}
+            data-last={props.isLast}
+        >
+            {/* <div className="work-img-bg rounded-full flex justify-center absolute p-6">
                                     <Image
                                         src={items.imgSrc}
                                         alt={items.imgSrc}
@@ -66,27 +80,23 @@ export default function Activities() {
                                         height={44}
                                     />
                                 </div> */}
-                            <div>
-                                <Image
-                                    src={"/images/Work/bg-arrow.svg"}
-                                    alt="arrow-bg"
-                                    width={85}
-                                    height={35}
-                                />
-                            </div>
-                            <h3 className="text-2xl text-offwhite font-semibold text-center mt-8">
-                                {items.heading}
-                            </h3>
-                            <p className="text-base font-normal text-bluish text-center mt-2">
-                                {items.subheading}
-                            </p>
-                            <span className="text-base font-normal m-0 text-bluish text-center hides">
-                                {items.hiddenpara}
-                            </span>
-                        </div>
-                    );
-                })}
+            <div>
+                <Image
+                    src={"/images/Work/bg-arrow.svg"}
+                    alt="arrow-bg"
+                    width={85}
+                    height={35}
+                />
             </div>
+            <h3 className="text-2xl text-offwhite font-semibold text-center mt-8">
+                {props.title}
+            </h3>
+            <p className="text-base font-normal text-bluish text-center mt-2">
+                {props.subtitle}
+            </p>
+            {/* <span className="text-base font-normal m-0 text-bluish text-center hides">
+                {items.hiddenpara}
+            </span> */}
         </div>
     );
-};
+}
